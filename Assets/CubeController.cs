a22427264ea02b16aa07af5003f8a5784683025c
tinamini <atsushi.matsui.7865@gmail.com>
2018-06-17 23:29:14 +0900

    Lesson7 Chapter5 キューブ配置を実装
    キューブを縦置きで配置する処理を追加

---
+++ Assets/CubeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+
+/// </summary>
+public class CubeController : MonoBehaviour
+{
+	#region Fields
+	#endregion
+
+	#region Properties
+	/// <summary>
+	/// キューブ移動速度
+	/// </summary>
+	public float Speed { get; private set; } = -0.2f;
+
+	/// <summary>
+	/// 消滅位置
+	/// </summary>
+	public float DeadLine { get; } = -10;
+	#endregion
+
+	#region Methods
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Awake()
+	{
+
+	}
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start ()
+	{
+		
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update ()
+	{
+		// キューブ移動
+		transform.Translate(this.Speed, 0, 0);
+
+		if(transform.position.x < this.DeadLine)
+		{
+			Destroy(gameObject);
+		}
+	}
+	#endregion
+}
---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+
+/// </summary>
+public class CubeGenerator : MonoBehaviour
+{
+	#region Fields
+	public GameObject CubePrefab;
+	#endregion
+
+	#region Properties
+	public float Delta { get; private set; } = 0;
+
+	public float Span { get; private set; } = 1.0f;
+
+	public float GeneratePosisionX { get; private set; } = 12.0f;
+
+	public float OffsetY { get; private set; } = 0.3f;
+
+	public float SpaceY { get; private set; } = 6.9f;
+
+	public float OffsetX { get; private set; } = 0.5f;
+
+	public float SpaceX { get; private set; } = 0.4f;
+
+	public int MaxBlockNum { get; private set; } = 4;
+	#endregion
+
+	#region Methods
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Awake()
+	{
+		
+	}
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start ()
+	{
+
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update ()
+	{
+		this.Delta += Time.deltaTime;
+		if (this.Delta > this.Span)
+		{
+			this.Delta = 0;
+			int num = Random.Range(1, MaxBlockNum + 1);
+
+			// 指定した数だけキューブを生成
+			for(int i = 0; i < num; i++)
+			{
+				var gameObject = Instantiate(CubePrefab) as GameObject;
+				gameObject.transform.position = new Vector2(this.GeneratePosisionX, i * this.SpaceY + this.OffsetY);
+			}
+			// 次のキューブまでの生成時間を決める
+			this.Span = this.OffsetX + this.SpaceX * num;
+		}
+	}
+	#endregion
+}
